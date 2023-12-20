0974cc7532328209814bae3537068d4abadd8ad5
longpt271 <longpt271@gmail.com>
2023-12-20 22:38:43 +0700

    add input search banner

--- src/pages/home/container/Banner.jsx
+++ src/pages/home/container/Banner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { images } from "../../../constants";
+import { FiSearch } from "react-icons/fi";
 
 const Banner = () => {
   return (
@@ -11,16 +12,33 @@ const Banner = () => {
       />
       <div className="container p-5 absolute bottom-10 left-1/2 transform -translate-x-1/2 flex justify-center">
         <div className="w-full lg:w-3/4 p-[10px] bg-[#2a292991] rounded-md text-white text-center">
-          <h2 className="font-ebgaramond text-xl lg:text-2xl leading-7 font-bold">
-            NƠI HẠNH PHÚC NGẬP TRÀN
-          </h2>
+          <div className="p-[10px]">
+            <h2 className="font-ebgaramond text-xl lg:text-2xl leading-7 font-bold">
+              NƠI HẠNH PHÚC NGẬP TRÀN
+            </h2>
 
-          <div className="bg-[#fab702] w-[80px]  h-[3px] my-[3px] mx-auto">
-            &nbsp;
+            <div className="bg-[#fab702] w-[80px]  h-[3px] my-[3px] mx-auto">
+              &nbsp;
+            </div>
+
+            <div className="text-xs lg:text-sm">
+              Hạnh phúc là có người đồng hành gửi trao tin cậy.
+            </div>
           </div>
 
-          <div className="text-xs lg:text-sm">
-            Hạnh phúc là có người đồng hành gửi trao tin cậy.
+          <div className="flex flex-col md:flex-row gap-2.5 p-1 font-normal text-[#959EAD] text-xs lg:text-sm">
+            <div className="w-full relative">
+              <FiSearch className="absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6" />
+              <input
+                className="w-full rounded-sm pl-12 pr-3 py-2 focus:outline-none shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]"
+                type="text"
+                placeholder="Từ khóa tìm kiếm"
+              />
+            </div>
+
+            <button className="w-full md:w-fit whitespace-nowrap bg-[#dca447] text-white rounded-lg md:rounded-sm px-5 py-3 md:py-0">
+              Tìm kiếm
+            </button>
           </div>
         </div>
       </div>
